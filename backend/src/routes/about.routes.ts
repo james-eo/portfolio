9650c23215665418fa6bf260dd48c7cc8892672d
james-eo <jamesejike04@gmail.com>
2025-08-05 08:50:02 +0100

    feat: Added createAbout and deleteAbout functionalities

--- backend/src/routes/about.routes.ts
+++ backend/src/routes/about.routes.ts
@@ -1,9 +1,14 @@
 import express from "express"
-import { getAbout, updateAbout } from "../controllers/about.controller"
+import { getAbout, createAbout, updateAbout, deleteAbout } from "../controllers/about.controller"
 import { protect, authorize } from "../middleware/auth.middleware"
 
 const router = express.Router()
 
-router.route("/").get(getAbout).post(protect, authorize("admin"), updateAbout)
+router
+  .route("/")
+  .get(getAbout)
+  .post(protect, authorize("admin"), createAbout)
+  .put(protect, authorize("admin"), updateAbout)
+  .delete(protect, authorize("admin"), deleteAbout)
 
 export default router
