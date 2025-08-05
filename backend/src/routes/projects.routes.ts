18da4414ee464112c5b6abb20637e250f7611387
james-eo <jamesejike04@gmail.com>
2025-08-05 08:43:56 +0100

    fix: Modified variable names and formatting

--- backend/src/routes/projects.routes.ts
+++ backend/src/routes/projects.routes.ts
@@ -1,21 +1,24 @@
-import express from "express"
+import express from "express";
 import {
   getProjects,
-  getProject,
+  getProjectById,
   createProject,
   updateProject,
   deleteProject,
-} from "../controllers/projects.controller"
-import { protect, authorize } from "../middleware/auth.middleware"
+} from "../controllers/projects.controller";
+import { protect, authorize } from "../middleware/auth.middleware";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/").get(getProjects).post(protect, authorize("admin"), createProject)
+router
+  .route("/")
+  .get(getProjects)
+  .post(protect, authorize("admin"), createProject);
 
 router
   .route("/:id")
-  .get(getProject)
+  .get(getProjectById)
   .put(protect, authorize("admin"), updateProject)
-  .delete(protect, authorize("admin"), deleteProject)
+  .delete(protect, authorize("admin"), deleteProject);
 
-export default router
+export default router;
