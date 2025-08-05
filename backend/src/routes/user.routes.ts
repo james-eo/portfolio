238623c5931e507f2b60e92799247028d32eb586
james-eo <jamesejike04@gmail.com>
2025-08-05 08:47:20 +0100

    fix: Used ESLint Formatting

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,15 +1,18 @@
-import express from "express"
-import { getUsers, getUser, createUser, updateUser, deleteUser } from "../controllers/user.controller"
+import express from "express";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller";
+import { protect, authorize } from "../middleware/auth.middleware";
 
-import { protect, authorize } from "../middleware/auth.middleware"
+const router = express.Router();
 
-const router = express.Router()
+router.use(protect);
+router.use(authorize("admin"));
+router.route("/").get(getUsers).post(createUser);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
-router.use(protect)
-router.use(authorize("admin"))
-
-router.route("/").get(getUsers).post(createUser)
-
-router.route("/:id").get(getUser).put(updateUser).delete(deleteUser)
-
-export default router
+export default router;
